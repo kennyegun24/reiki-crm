f0b05d89c3689cd7639354d624f2089431ad6b9a
Kennyegun24 <kennyegun240@gmail.com>
2024-05-06 00:01:48 +0100

    Forms to add new product and service

--- app/add/product/page.jsx
+++ app/add/product/page.jsx
@@ -1,116 +1,63 @@
-"use client";
-import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import React from "react";
+import "./style.css";
 import { FaPlus } from "react-icons/fa6";
-import { GoPlus } from "react-icons/go";
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-    color: theme.palette.common.white,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
-
-function createData(product_name, price, in_stock, sold) {
-  return { product_name, price, in_stock, sold };
-}
-
-const rows = [
-  createData("Scent relaxing candles", 120, 5, 29),
-  createData("Mat stretcher", 99.99, 1, 4),
-  createData("Essential Oils", 18.24, 4, 291),
-  createData("Crystals", 290.99, 1, 16),
-  createData("Massage table", 299.99, 29, 10),
-  createData("Massage chair", 300, 12, 32),
-  createData("Jazz Sound Collections", 15, 129, 320),
-];
-
-export default function CustomizedTables() {
+const page = () => {
   return (
-    <div
-      style={{
-        height: 600,
-        width: "100%",
-        padding: "2rem",
-        color: "#fff",
-        display: "flex",
-        flexDirection: "column",
-        gap: "2rem",
-      }}
-    >
-      <TableContainer
-        component={Paper}
-        sx={{ minWidth: 700, maxHeight: 500, border: "1px solid #91C3F4" }}
-      >
-        <Table stickyHeader aria-label="customized table">
-          <TableHead>
-            <TableRow>
-              <StyledTableCell sx={{ minWidth: "250px" }}>
-                Product Name
-              </StyledTableCell>
-              <StyledTableCell align="left" sx={{ minWidth: "100px" }}>
-                Price
-              </StyledTableCell>
-              <StyledTableCell sx={{ minWidth: "150px" }} align="center">
-                In Stock
-              </StyledTableCell>
-              <StyledTableCell sx={{ minWidth: "200px" }} align="left">
-                Sold
-              </StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <StyledTableRow key={row.product_name}>
-                <StyledTableCell component="th" scope="row">
-                  {row.product_name}
-                </StyledTableCell>
-                <StyledTableCell align="left">{row.price}</StyledTableCell>
-                <StyledTableCell align="center">{row.in_stock}</StyledTableCell>
-                <StyledTableCell align="left">{row.sold}</StyledTableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <button
-        style={{
-          background: "#91C3F4",
-          padding: "1rem 1.5rem",
-          border: "none",
-          width: "fit-content",
-          fontWeight: 700,
-          borderRadius: "8px",
-          alignSelf: "center",
-          display: "flex",
-          gap: "0.5rem",
-          alignItems: "center",
-          cursor: "pointer",
-        }}
-      >
-        <FaPlus style={{ background: "transparent" }} />
-        Add New Product
-      </button>
+    <div className="new_form_container">
+      <form>
+        <h1>Add new product</h1>
+        <hr />
+        <div className="horizontal_inputs_container">
+          <div>
+            <label htmlFor="">Product Name:</label>
+            <input placeholder="Product name" type="text" name="" id="" />
+            <p>Enter a valid product name here</p>
+          </div>
+          <div>
+            <label htmlFor="">Product Price:</label>
+            <input placeholder="Price" type="number" name="" id="" />
+            <p>Enter a valid number here</p>
+          </div>
+        </div>
+        <div className="horizontal_inputs_container">
+          <div>
+            <label htmlFor="">In Stock:</label>
+            <input placeholder="In stock" type="number" name="" id="" />
+            <p>How many of this product is in stock?</p>
+          </div>
+          <div>
+            <label htmlFor="">Product Description:</label>
+            <textarea
+              placeholder="Product Description..."
+              type="number"
+              name=""
+              id=""
+            />
+            <p>Description of the product</p>
+          </div>
+        </div>
+        <button
+          style={{
+            background: "#91C3F4",
+            padding: "1rem 1.5rem",
+            border: "none",
+            width: "fit-content",
+            fontWeight: 700,
+            borderRadius: "8px",
+            alignSelf: "center",
+            display: "flex",
+            gap: "0.5rem",
+            alignItems: "center",
+            cursor: "pointer",
+          }}
+        >
+          <FaPlus style={{ background: "transparent" }} />
+          Add New Product
+        </button>
+      </form>
     </div>
   );
-}
+};
+
+export default page;
--- app/add/service/page.jsx
+++ app/add/service/page.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { FaPlus } from "react-icons/fa6";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="new_form_container">
+      <form>
+        <h1>Add New Service</h1>
+        <hr />
+        <div className="horizontal_inputs_container">
+          <div>
+            <label htmlFor="">Service:</label>
+            <input placeholder="In stock" type="text" name="" id="" />
+            <p>What service is that?</p>
+          </div>
+          <div>
+            <label htmlFor="">Service Description:</label>
+            <textarea placeholder="Service Description..." name="" id="" />
+            <p>Description of the service</p>
+          </div>
+        </div>
+        <button
+          style={{
+            background: "#91C3F4",
+            padding: "1rem 1.5rem",
+            border: "none",
+            width: "fit-content",
+            fontWeight: 700,
+            borderRadius: "8px",
+            alignSelf: "center",
+            display: "flex",
+            gap: "0.5rem",
+            alignItems: "center",
+            cursor: "pointer",
+          }}
+        >
+          <FaPlus style={{ background: "transparent" }} />
+          Add New Service
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default page;
