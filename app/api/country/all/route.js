d75f95465d010e8a8a168acd5321799b2eff4e03
Kennyegun24 <kennyegun240@gmail.com>
2024-05-20 22:02:26 +0100

    Make responses dynamic

--- app/api/country/all/route.js
+++ app/api/country/all/route.js
@@ -3,6 +3,8 @@ import countrySchema from "@/models/country_schema";
 import { NextResponse } from "next/server";
 
 // save country
+export const dynamic = "force-dynamic";
+
 export const GET = async (req, res) => {
   try {
     await connectMongoDb();
