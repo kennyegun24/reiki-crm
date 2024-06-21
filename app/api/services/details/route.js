7ebd4f3c9278c5d2f173975a19eac9b6acfe0204
Kennyegun24 <kennyegun240@gmail.com>
2024-06-21 09:48:11 +0100

    Remove clg

--- app/api/services/details/route.js
+++ app/api/services/details/route.js
@@ -7,8 +7,6 @@ export const dynamic = "force-dynamic";
 export const GET = async (req, res) => {
   const { searchParams } = new URL(req.url);
   const id = searchParams.get("id");
-  // const { id } = req.query;
-  console.log(id);
   try {
     await connectMongoDb();
     const findProduct = await serviceSchema.findById(id);
